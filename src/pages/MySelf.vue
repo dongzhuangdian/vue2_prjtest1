93feb4e5156ed4c8b3a33822ac98d4ab8eeb9c0f
dongzhuangdian <1032010805@qq.com>
2021-12-21 13:48:12 +0800

    debug]

--- src/pages/MySelf.vue
+++ src/pages/MySelf.vue
@@ -2,10 +2,41 @@
 	<div id="myself">
 		<van-image round  width="5rem" height="5rem" src="https://img01.yzcdn.cn/vant/cat.jpeg"/>
 		
+		<van-cell value="全部" is-link>
+		  <!-- 使用 title 插槽来自定义标题 -->
+		  <template #title>
+		    <span class="custom-title">我的订单</span>
+		    <!-- <van-tag type="danger">标签</van-tag> -->
+		  </template>
+		</van-cell>
+		
+		<van-grid clickable :column-num="5">
+		  <van-grid-item icon="credit-pay" text="待付款" to="/" />
+		  <van-grid-item icon="shop-collect-o" text="待发货" url="/vant/mobile.html" />
+		  <van-grid-item icon="logistics" text="待收货" url="/vant/mobile.html" />
+		  <van-grid-item icon="chat-o" text="待评价" url="/vant/mobile.html" />
+		  <van-grid-item icon="refund-o" text="退款/售后" url="/vant/mobile.html" />
+		</van-grid>
+
+		
+	<!-- 	<van-cell title="单元格" icon="shop-o">
+		 
+		  <template #right-icon>
+		    <van-icon name="search" class="search-icon" />
+		  </template>
+		</van-cell> -->
+		
+
+		
+		<br>
 		<!-- <van-cell title="URL 跳转" is-link url="/vant/mobile.html" /> -->
-		<van-cell-group id=cell title="个人信息">
-			<van-cell title="路由跳转" is-link to="index" />
-		</van-cell-group>
+		<div class="cell">
+			<van-cell-group>
+				<van-cell title="个人信息" is-link to="index" />
+				<van-cell title="收货地址" is-link to="index" />
+				<van-cell title="联系客服" is-link to="index" />				
+			</van-cell-group>
+		</div>
 		
 	</div>
 </template>
@@ -13,6 +44,8 @@
 <script>
 	import { Image as VanImage } from 'vant';
 	import { Cell, CellGroup } from 'vant';
+	import { Icon } from 'vant';
+    import { Grid, GridItem } from 'vant';
 	export default {
 		name: "MySelf",
 		data(){
@@ -24,6 +57,9 @@
 			[VanImage.name] : VanImage,
 			[Cell.name] : Cell,
 			[CellGroup.name] : CellGroup,
+			[Icon.name] : Icon,
+			[Grid.name] : Grid,
+			[GridItem.name] : GridItem,
 			
 		}
 
@@ -31,8 +67,18 @@
 </script>
 
 <style lang="less">
-  #cell{
+  .cell{
 	  text-align:left
+  }
+  .custom-title {
+    // margin-right: 4px;
+    // vertical-align: left;
+	 text-align:left
+  }
+
+  .search-icon {
+    font-size: 16px;
+    // line-height: inherit;
   }
 
 </style>
